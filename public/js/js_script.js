b00e65c562440454d031caeaaa98c540680dd01a
Marcus <marcus.nordstrom.9240@student.uu.se>
2019-02-11 21:06:50 +0100

    javascript done! working on vue

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -10,43 +10,91 @@ function PrintContentBurger(burger){
     document.write("Cool burger name:"+burger.name +" "+burger.kcal+"kcal, WoW! look at that powerlevel");
 }
 
-
-
-//var myFather = new Person("JohnANDTEST", "Doe", 50, "blue");
-//dument. getelement by id
-//createlement div
-
-// var name = textNode(food[i].name)
-
-//createlemen p
-//title.appendChild(name1)
-
-var food1 = '{"name": "Basic","kCal": 500,"lactose": true,"gluten": true,"img":"https://www.language-trainers.de/blog/wp-content/uploads/2013/11/hamburger-1.jpg"}'
-
-
-var json = JSON.parse(food1);
-
-var innerDivOne = document.createElement('DIV');
-
-var title = document.createTextNode(json.name);
-var titleSize = document.createElement("H3")
-titleSize.appendChild(title);
-innerDivOne.appendChild(titleSize);
-
-var list = document.createElement('UL');
-
-var firstDot = document.createElement('LI');
-var kcal = document.createTextNode(json.kCal);
-firstDot.appendChild(kcal);
-list.appendChild(firstDot);
-
-if(json.lactose)
-{
-    var lactose = document.createTextNode("Lactose");
-    var secondDot = document.createElement('LI');
-    secondDot.appendChild(lactose);
-    list.appendChild(secondDot);
+//////////////////////////////////////////////////
+var submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click",logToConsoleButton);
+function logToConsoleButton(){
+    console.log("Button press");
+    createReturnArray();
 }
 
-innerDivOne.appendChild(list);
-document.getElementById("burgers").appendChild(innerDivOne);
+
+//Returns a array with all user info
+function createReturnArray(){
+    var radioButton = getRadioButton();
+    var returnArray = [
+        document.getElementById("fullname").value,
+        document.getElementById("email").value,
+        document.getElementById("street").value,
+        document.getElementById("house").value,
+        document.getElementById("PaymentOption").value,
+        radioButton
+    ];
+    for(var i = 0; i < food.length; i++){
+        if( document.getElementById(i).checked == true){
+            returnArray.push(document.getElementById(i).parentElement.parentElement.className);
+        }
+    }
+    alert(returnArray);
+    console.log(returnArray);
+    return returnArray;
+}
+
+
+function getRadioButton(){
+    var gender = document.getElementsByName('gender');
+    for(var i = 0; i < gender.length; i++){
+        if(gender[i].checked){
+            return gender[i].value;
+        }
+    }
+    
+}
+/*
+//////////////////////////////////////////////////
+for (i = 0; i < food.length; i++){
+    console.log(food[i].name);
+
+    var innerDivOne = document.createElement('DIV');
+    innerDivOne.setAttribute("class", food[i].name);
+
+    var title = document.createTextNode(food[i].name);
+    var titleSize = document.createElement("H3")
+    var checkbox = document.createElement("INPUT");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("id", i);
+    titleSize.appendChild(checkbox);
+    titleSize.appendChild(title);
+    innerDivOne.appendChild(titleSize);
+    var pic = document.createElement("IMG");
+    pic.setAttribute("src", food[i].img);
+    pic.setAttribute("width", "230");
+    pic.setAttribute("height", "230");
+    pic.setAttribute("alt", food[i].name);
+    innerDivOne.appendChild(pic);
+
+    var list = document.createElement('UL');
+
+    var firstDot = document.createElement('LI');
+    var kcal = document.createTextNode(food[i].kCal);
+    firstDot.appendChild(kcal);
+    list.appendChild(firstDot);
+
+    if(food[i].lactose)
+    {
+        var lactose = document.createTextNode("Lactose");
+        var secondDot = document.createElement('LI');
+        secondDot.appendChild(lactose);
+        list.appendChild(secondDot);
+    }
+    if(food[i].gluten)
+    {
+        var gluten = document.createTextNode("Gluten");
+        var thirdDot = document.createElement('LI');
+        thirdDot.appendChild(gluten);
+        list.appendChild(thirdDot);
+    }
+    innerDivOne.appendChild(list);
+    document.getElementById("allTheBurgers").appendChild(innerDivOne);
+}
+*/
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,8 @@
-var vm = new Vue({
-  el: '#myID',
-  data: {
-    arbitraryVariableName: 'Välj en burgare '+ new Date()
+let food_test = '{"name": "Basic","kCal": 500,"lactose": true,"gluten": true,"img":"https://www.language-trainers.de/blog/wp-content/uploads/2013/11/hamburger-1.jpg"}'
+
+new Vue({
+    el: '#Grid',
+    data: {
+      burgerMenu:food
   }
 })
